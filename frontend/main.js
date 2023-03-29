05fc99314b90782b430d0c40e04ae14b2f5085aa
cloudarcKnights <126310351+cloudarcKnights@users.noreply.github.com>
2023-03-28 17:12:17 -0700

    Wrote Javascript file for HTTP request

--- frontend/main.js
+++ frontend/main.js
@@ -2,25 +2,59 @@
 //     getVisitCount();
 // })
 
-// // const functionApiUrl = 'https://getresumecounter.azurewebsites.net/api/GetResumeCounter?code=Q/LkPt0mhQKdP8DoE3DdllAYdFQO//58Iq7AoS6JUBj9FgJ86Rqt8A==';
+// const UpdateVisitorCount = 'https://us-west2-infra-earth-377823.cloudfunctions.net/UpdateVisitorCount';
 // // const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
-const getVisitCount = () => {
-    let count = 30;
-    fetch(functionApiUrl).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count =  response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
-    });
-    return count;
-}
+// const getVisitCount = () => {
+//     let count = 30;
+//     fetch(UpdateVisitorCount).then(response => {
+//         return response.json()
+//     }).then(response =>{
+//         console.log("Website called function API.");
+//         count =  response.count;
+//         document.getElementById("counter").innerText = count;
+//     }).catch(function(error){
+//         console.log(error);
+//     });
+//     return count;
+// }
+// fetch('https://us-west2-infra-earth-377823.cloudfunctions.net/UpdateVisitorCount')
+//   .then(response => response.text())
+//   .then(data => {
+//     console.log(data);
+//   });
 
-fetch('https://us-west2-infra-earth-377823.cloudfunctions.net/UpdateVisitorCount')
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);
-  });
+
+// const visitorCount = 42;
+// const apiUrl = "https://counter-gateway-7u7o4lcy.wl.gateway.dev/UpdateVisitorCount";
+
+// fetch(apiUrl, {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({visitorCount: visitorCount})
+// })
+// .then(response => response.json())
+// .then(data => {
+//   console.log('Response:', data);
+// })
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
+
+
+const countEl = document.getElementById('vc');
+
+updateVisitCount();
+//Testing
+function updateVisitCount() {
+    fetch('https://counter-gateway-7u7o4lcy.wl.gateway.dev/UpdateVisitorCount')
+        .then(response => {
+        return response.json()
+      })
+        .then(data => {
+            console.log(data)
+            document.getElementById('count').innerHTML = data;
+        })
+    }
